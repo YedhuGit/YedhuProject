db05e4e6e0bd5a0447ebe8d8607a2733d880358d
YedhuGit <yedhuvijayan16@gmail.com>
2023-06-28 16:08:19 +0530

    Initial repository

---
+++ src/test/java/StepDefinition/LoginPage.java
@@ -0,0 +1,21 @@
+package StepDefinition;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+static WebDriver driver;
+public static void alertTest(){
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://demo.automationtesting.in/Alerts.html");
+
+}
+
+    public static void main(String[] args) {
+alertTest();
+    }
+}
